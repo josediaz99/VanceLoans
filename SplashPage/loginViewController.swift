a5723c65ee9a130437b22de350434c467c3f0704
jose <josediazisnow@gmail.com>
2019-08-06 21:09:53 -0500

    fixed keyboard

--- SplashPage/loginViewController.swift
+++ SplashPage/loginViewController.swift
@@ -2,15 +2,31 @@
 //  loginViewController.swift
 //  SplashPage
 //
-//  Created by SER-YCL7 on 7/15/19.
+//  Created by Central States SER 06 on 8/6/19.
 //  Copyright © 2019 CSApps. All rights reserved.
 //
 
 import UIKit
 
-class loginViewController:UIViewController {
+class loginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-}
-}
+
+        // Do any additional setup after loading the view.
+       
+      
+            self.hideKeyboardWhenTappedAround()
+        }    }
+    // Put this piece of code anywhere you like
+    extension UIViewController {
+        func hideKeyboardWhenTappedAround() {
+            let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+            tap.cancelsTouchesInView = false
+            view.addGestureRecognizer(tap)
+        }
+        
+        @objc func dismissKeyboard() {
+            view.endEditing(true)
+        }
+    }
